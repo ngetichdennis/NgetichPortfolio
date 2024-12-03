c9db0367395ffc036712135659c0e9530f671d21
Dennis <ngetichdennis90@gmail.com>
2024-12-03 20:53:24 +0300

    updated work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -194,7 +194,7 @@ const Work = () => {
                 <SwiperSlide key={index} className="w-full">
                   <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
                   <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
-                    <div className="relative w-full h-full">
+                    <div className="relative w-full h-full object-cover">
                       <Image
                         src={proj.image}
                         alt={proj.title}
